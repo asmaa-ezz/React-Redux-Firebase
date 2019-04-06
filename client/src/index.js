7619f41a8b19ff66796eba90bfe3caea46effecd
asmaa-ezz <asmadhoun@gmail.com>
2019-04-06 21:35:00 +0300

    Change the code in render to user module.hot change the index without render everytime

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
 serviceWorker.unregister();

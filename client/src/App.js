1bd864b4b91d24e2f1ef8db241f8379ada2e7577
asmaa-ezz <asmadhoun@gmail.com>
2019-04-07 13:04:21 +0300

    Add the Navbar component in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">Test</div>
+        <div className="App">
+          <Navbar />
+        </div>
       </BrowserRouter>
     );
   }

ec95c90834da2405358a8dae0938ba48e222ff82
asmaa-ezz <asmadhoun@gmail.com>
2019-04-11 15:57:21 +0300

    Add the Provider

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
 
 const store = createStore(rootReducer);
 
@@ -12,7 +13,12 @@ const rootEl = document.getElementById("root");
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 let render = () => {
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  );
 };
 
 if (module.hot) {

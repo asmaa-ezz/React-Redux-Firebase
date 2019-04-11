1c8aa4874f422b7f4d44d2828fec67b46e0aa47e
asmaa-ezz <asmadhoun@gmail.com>
2019-04-11 15:55:03 +0300

    import the rootReducer

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,13 @@ import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-const store = createStore();
+const store = createStore(rootReducer);
 
 const rootEl = document.getElementById("root");
 
+// ReactDOM.render(<App />, document.getElementById("root"));
 let render = () => {
   ReactDOM.render(<App />, rootEl);
 };
